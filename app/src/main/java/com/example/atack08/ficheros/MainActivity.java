579772a30cd3025b0a3feeca300fc82c53a222a5
atack08 <atack08@hotmail.es>
2016-11-27 23:48:31 +0100

    nada

--- app/src/main/java/com/example/atack08/ficheros/MainActivity.java
+++ app/src/main/java/com/example/atack08/ficheros/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.atack08.ficheros;
 
 import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,9 +28,12 @@ public class MainActivity extends AppCompatActivity {
 
         text = (EditText)findViewById(R.id.texto);
         consola = (TextView)findViewById(R.id.consola);
+
+        mostrarPanelError(Environment.getExternalStorageState());
         
     }
-    
+
+    //CREA Y ESCRIBE EN FICHERO - MEMORIA INTERNA
     public void  escribirEnFichero(View v){
 
         try {
@@ -37,9 +41,11 @@ public class MainActivity extends AppCompatActivity {
             OutputStreamWriter salida = new OutputStreamWriter(openFileOutput("ejercicio1.txt",
                     Context.MODE_APPEND));
 
-            String cadena = "\n" + text.getText().toString();
+
+            String cadena = text.getText().toString() + "\n";
 
             salida.write(cadena);
+
 
             salida.close();
             
@@ -51,15 +57,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //LEE EL CONTENIDO DEL FICHERO EN MEMORIA INTERNA Y LO IMPRIME EN EL TEXTVIEW
     public void leerFichero(View v){
 
         try {
             BufferedReader entrada = new BufferedReader(new InputStreamReader(
                     openFileInput("ejercicio1.txt")));
 
-            String linea = entrada.readLine();
+            consola.setText("");
 
-            mostrarPanelError(linea);
+            String linea;
+            while ((linea = entrada.readLine()) != null){
+                consola.append(linea + "\n");
+            }
+
 
             entrada.close();
 
@@ -69,6 +80,31 @@ public class MainActivity extends AppCompatActivity {
             mostrarPanelError(e.getLocalizedMessage());
         }
     }
+
+    //CREA Y ESCRIBE FICHERO EN MEMORIA EXTERNA - SD
+    public void escribirFicheroExterno(View v){
+
+        //COMPROBAMOS EL ESTADO DE LA MEMORIA EXTERNA - SD
+        String estado = Environment.getExternalStorageState();
+
+        //DEPENDIENDO DEL ESTADO HACEMOS UNA COSA U OTRA
+        switch (estado){
+
+            case Environment.MEDIA_MOUNTED:
+
+
+                break;
+
+            default:
+                mostrarPanelError("No se puede escribir en la tarjeta SD");
+                break;
+
+        }
+
+    }
+
+
+
     
     public void mostrarPanelError(String msg){
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
